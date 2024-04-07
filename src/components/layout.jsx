4168ef2196e452cd2408b0de8dedb7c71a5d497c
Kota-Ohno <manntamann0206@gmail.com>
2024-04-07 14:30:21 +0900

    layoutのMUI化

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,46 +1,58 @@
-import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-import {
-  container,
-  background,
-  header,
-  leftSidebar,
-  rightSidebar,
-  main,
-  footer,
-} from "./layout.module.css"
-import Header from "./header"
-import LeftSidebar from "./leftSidebar"
-import RightSidebar from "./rightSidebar"
-import Main from "./main"
-import Footer from "./footer"
+import React, { useState, useEffect, useRef } from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import Header from "./header";
+import LeftSidebar from "./leftSidebar";
+import RightSidebar from "./rightSidebar";
+import Main from "./main";
+import Footer from "./footer";
+import { Grid } from "@mui/material";
 
 // 余白部分を消去するのに必要
-import "../styles/styles.css"
+import "../styles/styles.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const headerRef = useRef(null); // ヘッダーのrefを作成
+  const [headerHeight, setHeaderHeight] = useState(0); // ヘッダーの高さを保存するステート
+
+  useEffect(() => {
+    // ヘッダーの高さを取得してステートを更新
+    if (headerRef.current) {
+      setHeaderHeight(headerRef.current.offsetHeight);
+    }
+  }, []); // 空の依存配列を指定して、コンポーネントのマウント時にのみ実行
+
   return (
-    <div className={container}>
+    <Grid container sx={{ height: '100vh', width: '100vw', alignItems: 'flex-start' }}>
       <StaticImage
-        className={background}
-        alt="スパゲティの背景"
+        alt="スパゲティ"
         src="../images/background.jpg"
-        height={1024} // モバイル時に背景を表示するのに必要？
+        style={{ position: "absolute", height: '100%', width: '100%', zIndex: -2 }}
       />
+      <Grid item xs={12} ref={headerRef}>
+        <Header></Header>
+      </Grid>
+      <Grid item md={2} sx={{ display: { xs: 'none', md: 'block' } }}>
+        <LeftSidebar></LeftSidebar>
+      </Grid>
+      <Grid item xs={12} md={8} sx={{
+        height: `calc(100vh - ${headerHeight}px)`, // ヘッダーの高さを使用
+        overflowY: 'scroll',
+        '-ms-overflow-style': 'none', // IE, Edge対応
+        'scrollbar-width': 'none', // Firefox対応
+        '&::-webkit-scrollbar': {
+          display: 'none', // Chrome, Safari対応
+        }
+      }}>
+        <Main pageTitle={pageTitle}>{children}</Main>
+      </Grid>
+      <Grid item md={2} sx={{ display: { xs: 'none', md: 'block' } }}>
+        <RightSidebar></RightSidebar>
+      </Grid>
+      <Grid item xs={12} sx={{ display: { md: 'none' } }}>
+        <Footer></Footer>
+      </Grid>
+    </Grid>
+  );
+};
 
-      <Header className={header}></Header>
-
-      <LeftSidebar className={leftSidebar}></LeftSidebar>
-
-      <RightSidebar className={rightSidebar}></RightSidebar>
-
-      <Main className={main} pageTitle={pageTitle}>
-        {children}
-      </Main>
-
-      <Footer className={footer}></Footer>
-    </div>
-  )
-}
-
-export default Layout
+export default Layout;
