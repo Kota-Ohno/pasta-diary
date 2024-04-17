0ef334c4fc3fe50ba9f1bb9dd6749986b055b6e1
Kota-Ohno <manntamann0206@gmail.com>
2024-04-17 21:51:19 +0900

    投稿リストのMUI化

--- src/components/postList.jsx
+++ src/components/postList.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "gatsby";
 import { linkText } from "./postList.module.css";
+import { List, ListItem, ListItemText } from "@mui/material";
 
 const PostList = ({ posts }) => (
-  <ul>
+  <List>
     {posts.map(({ node }) => (
-      <article key={node.id}>
-        <h2>
-          <Link className={linkText} to={`/blog/${node.frontmatter.slug}`}>
-            {node.frontmatter.title}
-          </Link>
-        </h2>
-        <p>投稿: {node.frontmatter.date}</p>
-      </article>
+      <ListItem key={node.id} alignItems="flex-start">
+        <ListItemText
+          primary={
+            <Link className={linkText} to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          }
+          secondary={`投稿: ${node.frontmatter.date}`}
+        />
+      </ListItem>
     ))}
-  </ul>
+  </List>
 );
 
 export default PostList;
