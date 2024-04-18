0a990ae1993c0fafbe034fd11b4d19f030901ab2
Kota-Ohno <manntamann0206@gmail.com>
2024-04-18 15:19:38 +0900

    アバウトページのMUI化

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,14 +3,20 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+// MUI Componentsをインポート
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="あばうとみー">
-      <p>
-        昼ごはんにいつもパスタを食べています。
-        ねこも飼っています。
-      </p>
+      <Grid>
+        <Typography variant="body1" sx={{ margin: '2rem'}} >
+          昼ごはんにいつもパスタを食べています。
+          ねこも飼っています。
+        </Typography>
+      </Grid>
     </Layout>
   )
 }
